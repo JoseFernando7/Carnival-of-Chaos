1d53e0ed56445a5cce603fe8240223b8977934ff
SEVFCA <sevfca246@gmail.com>
2026-08-19 17:49:44 -0500

    [add] estados de ataque y seguimiento del enemigo

--- Assets/Scripts/Enemy/EnemyIA.cs
+++ Assets/Scripts/Enemy/EnemyIA.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public class EnemyIA : MonoBehaviour
@@ -5,7 +7,9 @@ public class EnemyIA : MonoBehaviour
     public enum State
     {
         Idle,
-        Runaway
+        Runaway,
+        FollorPlayer,
+        Attack
     }
 
     [Header("Movement")]
@@ -13,13 +17,19 @@ public class EnemyIA : MonoBehaviour
     [SerializeField] private float radiusOfRunaway = 5;
     [SerializeField] private float magnitude = 0.25f;
 
+    [SerializeField] private float attackCD = 2f;
+    [SerializeField] private float attackTimer;
+    [SerializeField] private bool canAttack = false;
+    private bool isAttacking = false;
+
     public Vector2 center = new Vector2(10f, -3f);
 
     private Vector2 targerPosition;
     private Vector2 towardsTarget;
 
 
-    private GameObject player;
+
+   [SerializeField] private GameObject player;
     private Rigidbody2D rb;
     private Animator _animator;
 
@@ -29,9 +39,14 @@ public class EnemyIA : MonoBehaviour
     {
         state = State.Runaway;
         rb = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
         _animator = GetComponent<Animator>();
         transform.rotation = Quaternion.Euler(0,180,0);
+        attackTimer = 0;
+        canAttack = false;
     }
 
     private void Start()
@@ -47,13 +62,15 @@ public class EnemyIA : MonoBehaviour
 
                 rb.linearVelocity = Vector3.zero;
                 _animator.SetFloat("Move", 0);
+                canAttack = false;
+                attackTimer = 0;
 
                 break;
 
             case State.Runaway:
-               
+
                 towardsTarget = targerPosition - (Vector2)transform.position;
-                if(towardsTarget.magnitude < magnitude)
+                if (towardsTarget.magnitude < magnitude)
                 {
                     CalculateRandomPosition();
                 }
@@ -64,7 +81,53 @@ public class EnemyIA : MonoBehaviour
                 _animator.SetFloat("Move", 1);
 
                 Debug.DrawLine(transform.position, targerPosition, Color.green);
-               break;
+                break;
+
+            case State.FollorPlayer:
+
+                attackTimer += Time.deltaTime;
+
+                if(attackTimer >=  attackCD)
+                {
+                    canAttack = true;
+                }
+
+                //El enemigo se mueve en posicion aleatoria pero alineado a la posicion del jugador
+                targerPosition = new Vector2(targerPosition.x, player.transform.position.y);
+                towardsTarget = targerPosition - (Vector2)transform.position;
+
+                if (towardsTarget.magnitude < magnitude)
+                {
+                    CalculateRandomPosition();
+                }
+
+                //rb.linearVelocity = towardsTarget * moveSpeed; Esto genera un empuje xdd
+                transform.position += (Vector3)towardsTarget.normalized * moveSpeed * Time.deltaTime;
+                Rotate();
+                _animator.SetFloat("Move", 1);
+
+                Debug.DrawLine(transform.position, targerPosition, Color.green);
+
+                float distanceWithPlayer = Vector3.Distance(transform.position, new Vector3(transform.position.x, player.transform.position.y, transform.position.z));
+                if(distanceWithPlayer < 0.5 && canAttack)
+                {
+                    attackTimer = 0;
+                    canAttack = false;
+                    state = State.Attack;
+                }
+
+
+                break;
+
+            case State.Attack:
+                rb.linearVelocity = Vector3.zero;
+                _animator.SetFloat("Move", 0);
+
+                if (!isAttacking)
+                {
+                    StartCoroutine(EnemyAttack());
+                }
+                break;
         }
     }
 
@@ -78,14 +141,8 @@ public class EnemyIA : MonoBehaviour
         //    targerPosition = -targerPosition;
         //}
 
-        Debug.Log(targerPosition);
+        //Debug.Log(targerPosition);
     }
-
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.yellow;    
-    //    Gizmos.DrawSphere(center, radiusOfRunaway);
-    //}
 
     private void Rotate()
     {
@@ -97,6 +154,21 @@ public class EnemyIA : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
+    }
+
+    private IEnumerator EnemyAttack()
+    {
+        isAttacking = true;
+        Debug.Log("Enemy atacando");
+        yield return new WaitForSeconds(0.2f);
+        state = State.FollorPlayer;
+        isAttacking = false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position , new Vector3(transform.position.x, player.transform.position.y, transform.position.z));
     }
 
 }
