1b31f0eeec058f048a6ee0c522a035d7726ddb03
SEVFCA <sevfca246@gmail.com>
2026-08-21 12:46:29 -0500

    [add] Ui de armas del player

--- Assets/Scripts/Attacks/SpawnAttack.cs
+++ Assets/Scripts/Attacks/SpawnAttack.cs
@@ -6,6 +6,8 @@ public class SpawnAttack : MonoBehaviour
     [Header("Attack Prefabs")]
     [SerializeField] private Attack[] attackPrefabs;
 
+    [SerializeField] private GameObject uiWeapons;
+
     private readonly List<Attack> activeAttacks = new List<Attack>();
 
     public void SpawnRandomAttack(Transform playerTransform)
@@ -19,14 +21,21 @@ public class SpawnAttack : MonoBehaviour
 
       if (attack is CannonAttack && playerTransform != null)
       {
+        uiWeapons.GetComponent<UiWeapons>().ActivateCanon(0);
         attack.transform.SetParent(playerTransform, false);
         attack.transform.localPosition = new Vector3(4.5f, 0f, 0f);
       }
 
       if (attack is ShoeAttack shoeAttack)
       {
+        uiWeapons.GetComponent<UiWeapons>().ActivateShoe(0);
         shoeAttack.SetPlayerTransform(playerTransform);
       }
+
+      if(attack is BombAttack bombAttack)
+        {
+            uiWeapons.GetComponent<UiWeapons>().ActivateDog(0);
+        }
 
       attack.Activate();
     }
@@ -41,6 +50,8 @@ public class SpawnAttack : MonoBehaviour
         }
       }
 
-      activeAttacks.Clear();
+        uiWeapons.GetComponent<UiWeapons>().Desactivate();
+
+        activeAttacks.Clear();
     }
 }
